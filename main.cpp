b1948d196ec27f47b20d9edb9e28e0ae4a15e0e8
rafliad <radenistri@gmail.com>
2024-01-01 20:14:20 +0700

    main change

--- main.cpp
+++ main.cpp
@@ -7,7 +7,18 @@
 #include "gym.hpp"
 
 int main(int argc, const char * argv[]) {
+    listFacility LF;
+    listMember LM;
+    initList(LF, LM);
     int inputUser = menu();
+    while (inputUser != 0){
+        if (inputUser == 1){
+            menu1(LF,LM);
+        }else if (inputUser == 2){
+            
+        }
+        menu();
+    }
     std::cout << inputUser;
     return 0;
 }
